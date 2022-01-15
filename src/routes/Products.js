c962e934f96f6c9873c6037425318893ae12abf6
dohung <dohuyhung12@gmail.com>
2022-01-15 21:31:44 +0700

    add page for display single product

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,6 +1,5 @@
 import React, { useReducer, createContext } from 'react'
 import { Outlet } from 'react-router-dom'
-import ProductList from '../components/ProductList'
 
 export const CartContext = createContext()
 
@@ -13,7 +12,7 @@ const Products = () => {
   return (
     <CartContext.Provider value={{ cart, setCart }}>
       <section className='section'>
-        <h1> Products</h1>
+        {/* <h1> Products</h1> */}
         <Outlet />
       </section>
     </CartContext.Provider>
--- src/routes/SingleProduct.js
+++ src/routes/SingleProduct.js
@@ -1,17 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-const StyledDiv = styled.section`
-  h1 {
-    font-size: 10rem;
+import { Link, useParams } from 'react-router-dom'
+import Loading from './Loading'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+const StyledSection = styled.section`
+  margin-top: 3rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+
+  img {
+    width: 40vw;
   }
 `
 
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+const BtnContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  button {
+    border: transparent;
+    background: transparent;
+    font-size: 1.5rem;
+  }
+`
+
+const url = 'https://fakestoreapi.com/products/'
+
 const SingleProduct = () => {
+  const param = useParams()
+  const id = param.id
+
+  const [quantity, setQuantity] = useState(1)
+  const [product, setProduct] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  const fetchData = async () => {
+    const response = await fetch(url + id)
+    const data = await response.json()
+    setLoading(false)
+    setProduct(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+    return () => {}
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+  const { description, image, price, title } = product
+
+  console.log(product)
   return (
-    <StyledDiv>
-      <h1>Hello from single product</h1>
-    </StyledDiv>
+    <>
+      <Link to='/products' className='btn'>
+        Back to products
+      </Link>
+      <StyledSection>
+        <div className='image-container'>
+          <img src={image} alt={title} />
+        </div>
+        <StyledDiv>
+          <h3>{title}</h3>
+          <h5>${price}</h5>
+          <p>{description}</p>
+
+          <BtnContainer>
+            <button
+              onClick={() => {
+                setQuantity(quantity - 1)
+              }}
+            >
+              <FaMinus />
+            </button>
+            <h3>{quantity}</h3>
+            <button>
+              <FaPlus
+                onClick={() => {
+                  setQuantity(quantity + 1)
+                }}
+              />
+            </button>
+          </BtnContainer>
+
+          <button className='btn'>Add to Cart</button>
+        </StyledDiv>
+      </StyledSection>
+    </>
   )
 }
 
